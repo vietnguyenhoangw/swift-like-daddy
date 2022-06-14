e4cbe3ce129f1b4b9492cb2eef2df61a5cc9fb56
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2022-06-14 09:41:32 +0700

    :tada: Init project

---
+++ SwiftLikeDaddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftLikeDaddy
+//
+//  Created by vietnguyenw on 6/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftLikeDaddy/SwiftLikeDaddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLikeDaddyApp.swift
+//  SwiftLikeDaddy
+//
+//  Created by vietnguyenw on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLikeDaddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
